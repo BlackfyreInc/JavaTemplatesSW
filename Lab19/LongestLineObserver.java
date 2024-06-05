3ce4e490977e870a2eaf0164bf6d5ff8584c7765
Igor Falendysh <74731862+BlackfyreInc@users.noreply.github.com>
2024-06-05 19:56:07 +0300

    Add files via upload

---
+++ Lab19/LongestLineObserver.java
@@ -0,0 +1,14 @@
+public class LongestLineObserver implements Observer {
+    private String longestLine = "";
+
+    @Override
+    public void update(String line) {
+        if (line.length() > longestLine.length()) {
+            longestLine = line;
+        }
+    }
+
+    public String getLongestLine() {
+        return longestLine;
+    }
+}
---
+++ Lab19/WordCountObserver.java
@@ -0,0 +1,16 @@
+import java.util.regex.Pattern;
+
+public class WordCountObserver implements Observer {
+    private int wordCount = 0;
+    private final Pattern pattern = Pattern.compile("\\s+");
+
+    @Override
+    public void update(String line) {
+        String[] words = pattern.split(line);
+        wordCount += words.length;
+    }
+
+    public int getWordCount() {
+        return wordCount;
+    }
+}
